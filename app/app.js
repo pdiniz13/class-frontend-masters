07def0b4a5a4d1f139e484c04bae24ec6f6af79d
Chris Mather <chris@eventedmind.com>
2015-05-28 09:57:16 -0500

    tour of meteor

---
+++ app/app.js
@@ -0,0 +1,31 @@
+Comments = new Mongo.Collection('comments');
+
+if (Meteor.isClient) {
+  Template.CommentList.helpers({
+    comments: function () {
+      return Comments.find();
+    }
+  });
+
+  Template.CommentAdd.events({
+    'submit form': function (e, tmpl) {
+      e.preventDefault();
+
+      var formEl = tmpl.find('form');
+      var commentEl = tmpl.find('[name=comment]');
+      var comment = commentEl.value;
+
+      Comments.insert({
+        login: 'cmather',
+        timestamp: new Date,
+        room: 'main',
+        comment: comment
+      });
+
+      formEl.reset();
+    }
+  });
+}
+
+if (Meteor.isServer) {
+}
