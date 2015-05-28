7305760fd0be4823bb737e28055c1d6cde6b8fe3
Chris Mather <chris@eventedmind.com>
2015-05-28 10:37:24 -0500

    add moment package

--- app/app.js
+++ app/app.js
@@ -4,6 +4,10 @@ if (Meteor.isClient) {
   Template.CommentList.helpers({
     comments: function () {
       return Comments.find();
+    },
+
+    formatTimestamp: function (timestamp) {
+      return moment(timestamp).calendar();
     }
   });
 
