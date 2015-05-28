558ce29cb4d0729c95ffee1a82bbb4c0e94861f2
Chris Mather <chris@eventedmind.com>
2015-05-28 16:35:26 -0500

    add auth and security

--- app/app.js
+++ app/app.js
@@ -45,6 +45,20 @@ if (Meteor.isClient) {
 }
 
 if (Meteor.isServer) {
+  Comments.allow({
+    insert: function (userId, doc) {
+      return !!userId;
+    },
+
+    update: function (userId, doc) {
+      return false;
+    },
+
+    remove: function (userId, doc) {
+      return false;
+    }
+  });
+
   Meteor.methods({
     callMe: function (name) {
       return "hello, " + name;
@@ -52,26 +66,9 @@ if (Meteor.isServer) {
   });
 
   Meteor.publish('allComments', function () {
-    var cursor = Comments.find();
-    var self = this;
-    var handle = cursor.observeChanges({
-      added: function (id, fields) {
-        self.added('comments', id, fields);
-      },
-
-      changed: function (id, fields) {
-        self.changed('comments', id, fields);
-      },
-
-      removed: function (id) {
-        self.removed('comments', id);
-      }
-    });
-
-    this.ready();
-
-    this.onStop(function () {
-      handle.stop();
-    });
+    if (this.userId)
+      return Comments.find();
+    else
+      return this.ready();
   });
 }
