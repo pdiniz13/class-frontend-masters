f95570236efa3391eaea2a93cfc743b50b4d3c51
Chris Mather <chris@eventedmind.com>
2015-05-29 13:42:57 -0500

    finish Name reactive data source

--- reactivity/client/app.js
+++ reactivity/client/app.js
@@ -1,22 +1,25 @@
-dep = new Tracker.Dependency;
-
 Name = {
   _value: 'Evented Mind',
 
+  _dep: new Tracker.Dependency,
+
   get: function () {
+    this._dep.depend();
     return this._value;
   },
 
   set: function (value) {
+    this._value = value;
+    this._dep.changed();
   }
 };
 
-c1 = Tracker.autorun(function (computation) {
+Tracker.autorun(function () {
   var name = Name.get();
   console.log(name);
 });
 
-c2 = Tracker.autorun(function () {
+Tracker.autorun(function () {
   var name = Name.get();
   console.log(name);
 });
