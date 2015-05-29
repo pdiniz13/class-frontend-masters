1af6ad8a6fb7fbbb9d5d7dc25477516cacffaa50
Chris Mather <chris@eventedmind.com>
2015-05-29 14:17:24 -0500

    add flack initial app

---
+++ flack/client/app.js
@@ -0,0 +1,128 @@
+/*****************************************************************************/
+/* Subscriptions */
+/*****************************************************************************/
+Meteor.subscribe('comments');
+Meteor.subscribe('users');
+//TODO
+
+/*****************************************************************************/
+/* Initial State */
+/*****************************************************************************/
+Session.set('activeRoom', 'main');
+Session.set('showInviteConfirm', false);
+
+/*****************************************************************************/
+/* RPC Methods */
+/*****************************************************************************/
+Meteor.methods({
+  inviteFriend: function (email) {
+    // TODO
+  }
+});
+
+/*****************************************************************************/
+/* Template Helpers */
+/*****************************************************************************/
+Template.Navigation.helpers({
+  rooms: function () {
+    return Rooms.find({name: {$ne: 'main'}}, {sort: {name: 1}});
+  },
+
+  isRoomActiveClass: function (room) {
+    // TODO
+  },
+
+  showInviteConfirm: function () {
+    // TODO
+  }
+});
+
+Template.Room.helpers({
+  activeRoom: function () {
+    // TODO
+  }
+});
+
+Template.RoomAddDialog.helpers({
+  showRoomAddDialog: function () {
+    // TODO
+  }
+});
+
+Template.CommentList.helpers({
+  comments: function () {
+    var room = Session.get('activeRoom');
+    return Comments.find({room: room}, {sort: {timestamp: 1}});
+  }
+});
+
+Template.CommentItem.helpers({
+  formattedTimestamp: function (timestamp) {
+    return moment(timestamp).calendar();
+  },
+
+  avatarUrl: function () {
+    var comment = this;
+    var user = Meteor.users.findOne({'profile.login': comment.login});
+    return user.profile.avatarUrl;
+  }
+});
+
+/*****************************************************************************/
+/* Template Events */
+/*****************************************************************************/
+Template.Navigation.events({
+  'click [data-room-add]': function (e, tmpl) {
+    // TODO
+  },
+
+  'click [data-room]': function (e, tmpl) {
+    var room = $(e.target).data('room');
+    Session.set('activeRoom', room);
+  },
+
+  'submit form[data-invite]': function (e, tmpl) {
+    e.preventDefault();
+
+    var form = e.target;
+    var email = tmpl.find('[name=email]').value;
+    form.reset();
+
+    Meteor.call('inviteFriend', email);
+  }
+});
+
+Template.RoomAddDialog.events({
+  'submit form': function (e, tmpl) {
+    e.preventDefault();
+
+    var form = tmpl.find('form');
+    var roomName = tmpl.find('input[name=room]').value;
+
+    form.reset();
+
+    if (roomName.trim() == "")
+      return;
+
+    if (Rooms.findOne({name: roomName}))
+      return;
+
+    Rooms.insert({
+      name: roomName
+    });
+
+    //TODO
+    Session.set('activeRoom', roomName);
+  },
+
+  'click [data-cancel]': function (e, tmpl) {
+    e.preventDefault();
+    var form = tmpl.find('form');
+    form.reset();
+    Session.set('showRoomAddDialog', false);
+  }
+});
+
+Template.CommentAdd.events({
+  // TODO implement submit form handler
+});
---
+++ flack/server/app.js
@@ -0,0 +1,100 @@
+/*****************************************************************************/
+/* Publish Functions */
+/*****************************************************************************/
+Meteor.publish('comments', function () {
+  return Comments.find({}, {sort: {timestamp: 1}})
+});
+
+Meteor.publish('users', function () {
+  return Meteor.users.find({}, {fields: {profile: 1}});
+});
+
+// TODO
+
+/*****************************************************************************/
+/* RPC Methods */
+/*****************************************************************************/
+Meteor.methods({
+  inviteFriend: function (email) {
+    var user = Meteor.user();
+    var url = Meteor.absoluteUrl() || "http://localhost:3000"
+
+    if (!user) {
+      throw new Meteor.Error("Not authorized to invite friends!");
+    }
+
+    Email.send({
+      to: email, 
+      from: user.profile.email,
+      subject: "Join Flack!",
+      html: "I'd like to invite you to join me in <a href='" + url + "' target='_blank'>Flack!</a>"
+    });
+  }
+});
+
+/*****************************************************************************/
+/* Accounts */
+/*****************************************************************************/
+Accounts.onCreateUser(function (options, user) {
+  options.profile.login = user.services.github.username;
+  options.profile.email = user.services.github.email;
+
+  var accessToken = user.services.github.accessToken;
+  var username = options.profile.login;
+
+  // add header Authorization: token <token>
+  var apiOptions = {
+    headers: {
+      'Authorization': 'token ' + accessToken,
+      'User-Agent': 'eventedmind-devel'
+    }
+  };
+
+  var url = 'https://api.github.com/users/' + username;
+  var response = HTTP.get(url, apiOptions);
+
+  options.profile.avatarUrl = response.data.avatar_url;
+  options.profile.githubId = response.data.id;
+  options.profile.url = response.data.html_url;
+
+  user.profile = options.profile;
+  return user;
+});
+
+/*****************************************************************************/
+/* Security */
+/*****************************************************************************/
+Comments.allow({
+  insert: function (userId, doc) {
+    // TODO
+  },
+
+  update: function (userId, doc) {
+    return false;
+  },
+
+  remove: function (userId, doc) {
+    return false;
+  }
+});
+
+Rooms.allow({
+  insert: function (userId, doc) {
+    // TODO
+  },
+
+  update: function (userId, doc) {
+    return false;
+  },
+
+  remove: function (userId, doc) {
+    return false;
+  }
+});
+
+/*****************************************************************************/
+/* Database Seeding */
+/*****************************************************************************/
+if (!Rooms.findOne({name: 'main'})) {
+  Rooms.insert({name: 'main'});
+}
